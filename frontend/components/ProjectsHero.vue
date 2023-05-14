cf00fef516f678a5bbe8e0d30cee5a177af31ad2
André Nunes <hello@anuness.dev>
2023-05-14 07:30:18 +0100

    Add projects page

---
+++ frontend/components/ProjectsHero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero min-h-screen bg-gray-950">
+    <div class="hero-content text-center">
+      <div class="max-w-xl">
+        <h1 class="text-5xl font-bold">{{ $t("project.hero.title") }}</h1>
+        <p class="py-6">
+          {{ $t("project.hero.description") }}
+        </p>
+        <div class="flex flex-col md:flex-row justify-center">
+          <button class="btn btn-primary btn-outline rounded-full">{{ $t("project.hero.btn") }}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
